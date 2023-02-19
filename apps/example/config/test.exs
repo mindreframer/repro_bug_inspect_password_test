56b28565c67773a6710c0b09d5e9d56fb1e1cd42
Roman Heinrich <roman.heinrich@gmail.com>
2023-02-19 23:38:21 +0100

    Add generated files from phx.gen.auth

--- apps/example/config/test.exs
+++ apps/example/config/test.exs
@@ -31,3 +31,7 @@ config :logger, level: :warning
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+
+# Only in tests, remove the complexity from the password hashing algorithm
+config :bcrypt_elixir, :log_rounds, 1
